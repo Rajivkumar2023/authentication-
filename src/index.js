5bcbb9112bd2ee53029f3d1f2feb0dbdf39ad54a
Rajiv kumar <rajivdhanama00@gmail.com>
2024-02-12 17:16:31 +0530

    adding new files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 express= require('express');
 path = require('path');
-phash = require('bcrypt');
+bcrypt = require('bcrypt');
 Student = require('./database');
 app = express();
 port = 3000;
@@ -11,20 +11,54 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
-app.get('/',(req, res)=>{
+app.get('/login',(req, res)=>{
     res.render('login')
 })
 
+////this is for after loing home page redrection
+app.get('/home', (req, res)=>{
+    res.render('home')
+})
+
+////this for login username and password validation
+app.post('/', async (req, res) => {
+    try {
+        const checkUser = await Student.findOne({ username: req.body.username });
+        if (checkUser) {
+            const match = await bcrypt.compare(req.body.password, checkUser.password);
+            if (match) {
+                res.redirect('/home');  ///// after username and passsword match , then redirect home page
+            } else {
+                res.send('Incorrect password');
+            }
+        } else {
+            res.send('Username does not exist');
+        }   
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+/////this is for registration and rastration validation
 app.post('/register', async(req, res)=>{
     const { username, password }=  req.body;
-    console.log(username, password);
-    saltRounds = 1;
-    encpassword = phash.hash(password, saltRounds);
-    console.log(encpassword)
-    newStudent = new Student({username, encpassword});
-    Studentsave = await newStudent.save();
-    res.redirect('/register')
-})
+    
+    exitstingUser = await Student.findOne({username})
+    if(exitstingUser){res.send('User already exists, try to another user name')}
+
+    else{
+        encpassword = await bcrypt.hash(password, 4);
+        newStudent = new Student({
+            username : username,
+            password : encpassword
+        })
+
+        console.log(username, encpassword);
+        Studentsave = await newStudent.save();
+        res.redirect('/register')
+    }
+});
 
 app.get('/register', (req, res)=>{
     res.render('register')
